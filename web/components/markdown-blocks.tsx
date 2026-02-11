12965ef8f5bd5cede00e821a06aa05942e03546f
Jeremy <git@jrmyphlmn.com>
2026-02-11 11:46:28 -0800

    fix build errors

--- web/components/markdown-blocks.tsx
+++ web/components/markdown-blocks.tsx
@@ -2,26 +2,26 @@ import Link from "next/link";
 import { ReactNode } from "react";
 
 export const MarkdownBlocks = {
-  p: ({ children }: { children: ReactNode }) => {
+  p: ({ children }: { children?: ReactNode }) => {
     return <p className="leading-6 text-base">{children}</p>;
   },
-  ol: ({ children }: { children: ReactNode }) => {
+  ol: ({ children }: { children?: ReactNode }) => {
     return <ol className="leading-6">{children}</ol>;
   },
-  ul: ({ children }: { children: ReactNode }) => {
+  ul: ({ children }: { children?: ReactNode }) => {
     return <ul className="leading-6 flex flex-col gap-1">{children}</ul>;
   },
-  li: ({ children }: { children: ReactNode }) => {
+  li: ({ children }: { children?: ReactNode }) => {
     return <li className="leading-6 text-base list-disc">{children}</li>;
   },
-  code: ({ children }: { children: ReactNode }) => {
+  code: ({ children }: { children?: ReactNode }) => {
     return (
       <code className="text-sm font-mono p-0.5 px-1 border border-neutral-300 dark:border-neutral-800 rounded-md">
         {children}
       </code>
     );
   },
-  a: ({ children, href = "" }: { children: ReactNode; href: string }) => {
+  a: ({ children, href = "" }: { children?: ReactNode; href?: string }) => {
     return (
       <Link
         href={href}
